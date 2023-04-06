4f0e0b11b4da7224bbab8a9b6803ac60a93878d6
ErmiyasAbate <ermiyasabateg@gmail.com>
2023-04-06 20:37:52 +0300

    README.md is updated!

---
+++ 01 Basic JavaScript/04 JavaScript Case Sensitivity.js
@@ -0,0 +1,8 @@
+/* In JavaScript all variable and function names are case-insensitive.
+MYVAR is not the same as MyVar or myvar*/
+
+/* The best practice in writing JavaScript variables
+is using Camel Case */
+var someVariable;
+var anotherVariableName;
+var thisVariableNameIsSoLong;
