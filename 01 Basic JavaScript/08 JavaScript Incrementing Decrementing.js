39385cc7a7725215ee669ca9f21dc9c62d9fa7d1
ErmiyasAbate <ermiyasabateg@gmail.com>
2023-04-06 22:41:35 +0300

    JavaScript - Incrementing/Decrementing

--- 01 Basic JavaScript/08 JavaScript Incrementing Decrementing.js
+++ 01 Basic JavaScript/08 JavaScript Incrementing Decrementing.js
@@ -5,3 +5,12 @@ let myVar = 87;
 myVar++;
 console.log(myVar);
 // The result will be 88
+
+
+/* We can easily decrement or decrease a variable by one 
+with the -- operator.
+i--; is the equivalent of i = i - 1; */
+let result = 54;
+result--;
+console.log(result);
+// The result will be 53
