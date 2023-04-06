16237e658c52cdcdf072a3d6162d547eaf776f59
ErmiyasAbate <ermiyasabateg@gmail.com>
2023-04-06 23:37:23 +0300

    JavaScript - String Immutability

---
+++ 01 Basic JavaScript/14 JavaScript String Immutability.js
@@ -0,0 +1,10 @@
+/* In JavaScript, String values are immutable, which means 
+that they cannot be altered once created. */
+let myStr = "Bob";
+myStr[0] = "J";
+/* This produces an error, because the letter B in the string
+Bob cannot be changed to the letter J. But we can change by using
+the following
+let myStr = "Bob";
+myStr = "Job";
+*/
