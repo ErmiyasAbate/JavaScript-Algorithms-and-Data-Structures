88ea33c600c077ddd89ed3013c0ddf62d977ce34
ErmiyasAbate <ermiyasabateg@gmail.com>
2023-04-06 22:09:11 +0300

    JavaScript - var VS let

--- 01 Basic JavaScript/05 JavaScript var VS let.js
+++ 01 Basic JavaScript/05 JavaScript var VS let.js
@@ -1,4 +1,13 @@
-var camper = "James";
-var camera = "David";
+/* One of the problem with var keyword arguments is that 
+it is easily overwrite the variable declaration.*/
+// var camper = "James";
+// var camper = "David";
+// console.log(camper);
+// The output shows "David" because "James" is overwritten.
 
+/* But the keyword called "let" is a major update to JavaScript,
+and it solves the potential issue with the var keyword */
+let camper = "James";
+let camper = "David";
 console.log(camper);
+// The result is error messages.
