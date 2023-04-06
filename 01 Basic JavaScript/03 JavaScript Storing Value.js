37e6a5421af52085d5542b38814348e55bd13d9f
ErmiyasAbate <ermiyasabateg@gmail.com>
2023-04-06 20:29:49 +0300

    JavaScript - Storing Values

---
+++ 01 Basic JavaScript/03 JavaScript Storing Value.js
@@ -0,0 +1,21 @@
+/* In Javascript, we can store a value in a variable with
+the assignment operator */
+var a;
+a = 7;
+
+/* If there are any calculations to the right
+ot the assignment operator (=), those are performed before the value assigned
+to the variable on the left side of the operator */
+var b;
+b = 4 * 8;
+
+/* We can assign the value of the variable to another variable
+using the assignment operator */
+var myVar;
+myVar = 5;
+var myNum;
+myNum = myVar;
+
+/* It is common to initialize a variable to an initial value
+in the same line as it is declared */
+var myVar = 7;
