248b21850e4d3e6c6d9ce2e09e46cab0f2fb2a68
ErmiyasAbate <ermiyasabateg@gmail.com>
2023-04-06 23:16:09 +0300

    JavaScript - Concatenating Strings

---
+++ 01 Basic JavaScript/11 JavaScript Concatenating Strings.js
@@ -0,0 +1,14 @@
+/* In JavaScript, when the + operator is used with a string value,
+it's called operator. */
+let fullName = "Ermiyas" + " Abate";
+console.log(fullName);
+
+// OR we can use this
+let ourStr = "I come first.";
+ourStr += " I come second.";
+console.log(ourStr);
+
+// OR we can use this
+let myName = "Ermiyas Abate";
+let myStr = "My name is " + myName + " and I am very well!";
+console.log(myStr);
