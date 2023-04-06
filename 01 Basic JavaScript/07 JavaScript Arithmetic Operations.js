c08c299635bd7a9b8e63cfcf7ce009959f33fefa
ErmiyasAbate <ermiyasabateg@gmail.com>
2023-04-06 22:29:49 +0300

    JavaScript - Arithmetic Operations

---
+++ 01 Basic JavaScript/07 JavaScript Arithmetic Operations.js
@@ -0,0 +1,10 @@
+// In JavaScript we can perform the following arithmetic operations
+let a = 10;
+let b = 20;
+let c = 23;
+
+console.log(a + b);
+console.log(b - a);
+console.log(b * a);
+console.log(b / a);
+console.log(c % a);
