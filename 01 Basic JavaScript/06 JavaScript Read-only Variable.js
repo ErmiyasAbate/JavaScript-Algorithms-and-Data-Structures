611a15738d28b3b82bc04bda61902872825b9a8d
ErmiyasAbate <ermiyasabateg@gmail.com>
2023-04-06 22:16:23 +0300

    JavaScript - Read-only Variables

---
+++ 01 Basic JavaScript/06 JavaScript Read-only Variable.js
@@ -0,0 +1,11 @@
+/* We can declare read-only variables using the "const" keyword.
+const has all the awesome features that "let" has, with the added
+bonus that the variable declared using the "const" keyword area read-only */
+const a = 6;
+a = 10;
+console.log(a);
+/* The console will display an error due to reassigning
+the value of a. 
+It is also common to use Uppercase variable identifiers
+for constant values
+*/
