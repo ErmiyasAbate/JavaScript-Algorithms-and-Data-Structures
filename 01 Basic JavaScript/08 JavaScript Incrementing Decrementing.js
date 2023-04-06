d4232b2cb3d64ff0dca9cd78e49f757514a3d044
ErmiyasAbate <ermiyasabateg@gmail.com>
2023-04-06 22:38:16 +0300

    JavaScript - Incrementing/Decrementing

---
+++ 01 Basic JavaScript/08 JavaScript Incrementing Decrementing.js
@@ -0,0 +1,8 @@
+/* We can easily increment or add one to a variable
+with the ++ operator. 
+i++; is the equivalent of i = i + 1; */
+
+let myVar = 87;
+myVar++;
+console.log(myVar);
+// The result will be 88
