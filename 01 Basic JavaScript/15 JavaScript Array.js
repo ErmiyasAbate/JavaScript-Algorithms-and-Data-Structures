9bbdab36b7059ae3114d03f46ae773a7454e2de5
ErmiyasAbate <ermiyasabateg@gmail.com>
2023-04-10 13:28:28 +0300

    JavaScript - Arrays

---
+++ 01 Basic JavaScript/15 JavaScript Array.js
@@ -0,0 +1,7 @@
+/* Using JavaScript array variable, we can easily store several 
+pieces of data in one place.
+
+We start an array declaration with an opening square bracket
+end it with closing square bracket. */
+let sandwich = ["Peanut butter", "Jelly", "Bread"];
+const animals = ["Elephant", "Lion", "Zebra", "Tiger", "Cheetah"];
