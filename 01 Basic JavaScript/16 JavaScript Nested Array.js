70ce1f45578ac5fc8ed86d65909bfdca37c842ac
ErmiyasAbate <ermiyasabateg@gmail.com>
2023-04-10 14:14:24 +0300

    JavaScript - Nested Array

---
+++ 01 Basic JavaScript/16 JavaScript Nested Array.js
@@ -0,0 +1,8 @@
+// We can also nest arrays within other arrays
+let teams = [
+    ["Arsenal", 70],
+    ["Manchester United", 50],
+    ["Liverpool", 40]
+];
+
+// This is also called a multi-dimensional array.
