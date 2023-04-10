4cba19f12888db650a441a8f477330361864f912
ErmiyasAbate <ermiyasabateg@gmail.com>
2023-04-10 19:55:51 +0300

    JavaScript - Functions

---
+++ 01 Basic JavaScript/24 JavaScript Function.js
@@ -0,0 +1,7 @@
+//In JavaScript, we can divide our code into reusable parts called functions.
+function functionName() {
+    console.log("Hello world");
+}
+
+// We can call or invoke this function by using its name followed by parentheses
+functionName();
