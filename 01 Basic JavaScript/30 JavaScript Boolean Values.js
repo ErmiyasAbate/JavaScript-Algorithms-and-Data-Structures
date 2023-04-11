94c6f96215623bfd1fb974f9234dffa5e3428ce8
ErmiyasAbate <ermiyasabateg@gmail.com>
2023-04-11 16:59:40 +0300

    JavaScript - Boolean Values

---
+++ 01 Basic JavaScript/30 JavaScript Boolean Values.js
@@ -0,0 +1,18 @@
+/*
+Booleans are data types may only be one of two values: true or false. 
+They are basically little on-off switches, where true is on and false is off. 
+These two states are mutually exclusive.
+
+Boolean values are never written with quotes. The strings "true" and "false" are 
+not Boolean and have no special meaning in JavaScript.
+*/
+
+function trueBoolean(){
+    return true;
+}
+trueBoolean();
+
+function falseBoolean(){
+    return false;
+}
+falseBoolean();
