36ae64243c634e1039da8b008e0245598fdf550d
Ermiyas Abate <ermiyasabateg@gmail.com>
2023-05-31 21:37:17 +0300

    JavaScript Switch Statement

--- 01 Basic JavaScript/42 JavaScript Switch Statement.js
+++ 01 Basic JavaScript/42 JavaScript Switch Statement.js
@@ -64,3 +64,40 @@ function fruits(val) {
 console.log(fruits(5)); // This will return Orange
 console.log(fruits(3));
 console.log(fruits(2));
+
+/* 
+    Multiple Identical Options
+    Question
+        Write a switch statement to set answer for the following ranges:
+            1-3 - Low
+            4-6 - Mid
+            7-9 - High
+*/
+
+function sequentialSizes(val) {
+    let answer = "";
+    // Only change code below this line
+
+    switch (val) {
+        case 1:
+        case 2:
+        case 3:
+            answer = "Low";
+            break;
+        case 4:
+        case 5:
+        case 6:
+            answer = "Mid";
+            break;
+        case 7:
+        case 8:
+        case 9:
+            answer = "High";
+            break;
+    }
+
+    // Only change code above this line
+    return answer;
+}
+
+console.log(sequentialSizes(3));
