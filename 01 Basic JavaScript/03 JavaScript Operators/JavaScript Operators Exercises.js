cba687320afdd961b382aae6a2c8384a9aea5049
Ermiyas Abate <ermiyasabateg@gmail.com>
2023-06-08 23:02:18 +0300

    JavaScript Operators Exercise

---
+++ 01 Basic JavaScript/03 JavaScript Operators/JavaScript Operators Exercises.js
@@ -0,0 +1,33 @@
+/*
+    Question 1
+        Multiply 10 with 5, and alert the result:
+*/
+alert(10 * 5);
+
+/*
+    Question 2
+        Divide 10 by 2, and alert the result:
+*/
+alert(10 / 2);
+
+/*
+    Question 3
+        Alert the remainder when 15 is divided by 9.
+*/
+alert(15 % 9);
+
+/*
+    Question 4
+        Use the correct assignment operator that will result in x being 15 (same as x = x + y).
+*/
+let x = 10;
+let y = 5;
+x += y;
+
+/*
+    Question 5
+        Use the correct assignment operator that will result in x being 50 (same as x = x * y).
+*/
+let x1 = 10;
+let y1 = 5;
+x1 *= y1;
