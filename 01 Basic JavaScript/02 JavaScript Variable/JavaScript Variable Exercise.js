69810565cf16140f075055cc8e3690975742d0be
Ermiyas Abate <ermiyasabateg@gmail.com>
2023-06-08 21:48:55 +0300

    JavaScript Variable Exercise

---
+++ 01 Basic JavaScript/02 JavaScript Variable/JavaScript Variable Exercise.js
@@ -0,0 +1,40 @@
+/*
+    Question 1
+        Create a variable called carName, assign the value Volvo to it.
+*/
+let carName = "Volvo";
+
+/*
+    Question 2
+        Create a variable called total, assign the value 50 to it.
+*/
+let total = 50;
+
+/*
+    Question 3
+        Display the sum of 5 + 10, using two variables: x and y.
+*/
+let x = 5;
+let y = 10;
+console.log("Total: ", x + y)
+
+/*
+    Question 4
+        Create a variable called z, assign x + y to it, and display the result in an alert box.
+*/
+
+let x1 = 5;
+let y1 = 10;
+let z = x1 + y1;
+
+alert(z);
+
+/*
+    Question 5
+        On one single line, declare three variables with the following names and values:
+            firstName = "John"
+            lastName = "Doe"
+            age = 35
+*/
+
+let firstName = "John", lastName = "Doe", age = 35;
