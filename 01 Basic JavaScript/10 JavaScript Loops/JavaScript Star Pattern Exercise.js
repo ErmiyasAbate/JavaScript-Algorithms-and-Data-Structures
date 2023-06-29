1f66223a32ea37cdb9738dc8b9035f66f06ef4d6
Ermiyas Abate <ermiyasabateg@gmail.com>
2023-06-29 13:45:52 +0300

    JavaScript Star Pattern Exercise

--- 01 Basic JavaScript/10 JavaScript Loops/JavaScript Star Pattern Exercise.js
+++ 01 Basic JavaScript/10 JavaScript Loops/JavaScript Star Pattern Exercise.js
@@ -1,16 +1,115 @@
-// I. Square Star Pattern in Javascript
-
+// --------------------------------------------------------------------------------------------------
+console.log('Square Star Pattern in Javascript');
 let squarePattern = "";
 
-function squareStarPattern (row, column) {
+function squareStarPattern(row, column) {
     for (var i = 0; i < row; i++) {
         for (let j = 0; j < column; j++) {
             squarePattern += "* ";
         }
         squarePattern += "\n";
     }
-    
+
     console.log(squarePattern);
 }
 
-squareStarPattern (5, 5)
+squareStarPattern(5, 5)
+
+// --------------------------------------------------------------------------------------------------
+console.log('Hollow Square Pattern');
+let hollowPattern = "";
+
+function hollowSquarePattern(row, column) {
+    for (var i = 0; i < row; i++) {
+        for (let j = 0; j < column; j++) {
+            if (i === 0 || i === row - 1) {
+                hollowPattern += "* ";
+            } else {
+                if (j === 0 || j === column - 1) {
+                    hollowPattern += "* ";
+                } else {
+                    hollowPattern += "  ";
+                }
+            }
+        }
+        hollowPattern += "\n";
+    }
+
+    console.log(hollowPattern);
+}
+
+hollowSquarePattern(5, 5)
+
+// --------------------------------------------------------------------------------------------------
+console.log('Right Triangle Pattern One in Javascript');
+let rightTriangle = "";
+
+function rightTrianglePattern(row) {
+    for (var i = 0; i < row; i++) {
+        for (let j = 0; j <= i; j++) {
+            rightTriangle += "* ";
+        }
+        rightTriangle += "\n";
+    }
+
+    console.log(rightTriangle);
+}
+
+rightTrianglePattern(5)
+
+console.log('Right Triangle Pattern Two in Javascript');
+let rightTriangleTwo = "";
+
+function rightTrianglePatternTwo(row) {
+    for (var i = 1; i <= row; i++) {
+        for (let j = 0; j < row - i; j++) {
+            rightTriangleTwo += "  ";
+        }
+        // printing star
+        for (let k = 0; k < i; k++) {
+            rightTriangleTwo += "* ";
+        }
+        rightTriangleTwo += "\n";
+    }
+
+    console.log(rightTriangleTwo);
+}
+
+rightTrianglePatternTwo(5)
+
+console.log('Downward Triangle Star Pattern');
+let downTriangle = "";
+
+function downTrianglePattern(row) {
+    for (let i = 0; i < row; i++) {
+        for (let j = 0; j < row - i; j++) {
+            downTriangle += "* ";
+        }
+        downTriangle += "\n";
+    }
+
+    console.log(downTriangle);
+}
+
+downTrianglePattern(5)
+
+console.log('Downward Triangle Star Pattern Two');
+let downTriangleTwo = "";
+
+function downTrianglePatternTwo(row) {
+    for (let i = 0; i < row; i++) {
+        for (let j = 0; j < i; j++) {
+            downTriangleTwo += "  ";
+        }
+
+        for (let k = 0; k < row - i; k++) {
+            downTriangleTwo += "* ";
+        }
+
+        downTriangleTwo += "\n";
+    }
+
+    console.log(downTriangleTwo);
+}
+
+downTrianglePatternTwo(5)
