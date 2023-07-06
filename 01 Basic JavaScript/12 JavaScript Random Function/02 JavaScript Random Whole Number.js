3c3b7f7144f25919dfb6ca7e59152c06177b25c5
Ermiyas Abate <ermiyasabateg@gmail.com>
2023-07-06 17:24:17 +0300

    JavaScript Random Function

---
+++ 01 Basic JavaScript/12 JavaScript Random Function/02 JavaScript Random Whole Number.js
@@ -0,0 +1,9 @@
+console.log(Math.floor(Math.random() * 20))
+
+/*
+    Math.random() to generate a random decimal number.
+
+    Multiply that random decimal number by 20.
+
+    Use Math.floor() to round this number down to its nearest whole number.
+*/
