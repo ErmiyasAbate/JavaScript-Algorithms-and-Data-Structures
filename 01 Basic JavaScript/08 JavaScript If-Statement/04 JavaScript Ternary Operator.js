d37cdce8bf7b65d5059db4001a2498c8f940f406
Ermiyas Abate <ermiyasabateg@gmail.com>
2023-07-06 23:55:10 +0300

    JavaScript JavaScript Ternary Operator

---
+++ 01 Basic JavaScript/08 JavaScript If-Statement/04 JavaScript Ternary Operator.js
@@ -0,0 +1,31 @@
+/*
+    ---------- Conditional (Ternary) Operator ----------
+
+    The conditional operator, also called the ternary operator, can be used as a one line if-else expression.
+
+    The syntax is a ? b : c, where a is the condition, b is the code to run when the condition returns true, 
+    and c is the code to run when the condition returns false.
+    
+    The following function uses an if/else statement to check a condition:
+
+    function findGreater(a, b) {
+        if(a > b) {
+            return "a is greater";
+        }
+        else {
+            return "b is greater or equal";
+        }
+    }
+
+    This can be re-written using the conditional operator:
+
+    function findGreater(a, b) {
+        return a > b ? "a is greater" : "b is greater or equal";
+    }
+*/
+
+function checkEquality(a, b) {
+    return a === b? "a is equal to b" : "a is not equal to b";
+}
+
+console.log(checkEquality(1, 2));
